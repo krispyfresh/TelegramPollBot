49309a5c7bacbc61f5981fabb0a048f8a0b832f1
Chris Parsons <chris.parsons@rackspace.com>
2017-12-09 16:08:10 -0600

    added a bunch of shit

--- PollBot.ps1
+++ PollBot.ps1
@@ -14,15 +14,44 @@ while($true -eq $true)
             "/newpoll *" # /newpoll "Question" "Option1" "Option2" etc.  start a new poll.  one poll at a time allowed in order to avoid spam
             { 
                 $query = $updates.result.message[$i].text.substring(9)
-                write-host $query
+                
+                if($query -match '"(.*?[^\\])') #this regex is not good, needs improvment
+                {
+                    write-host 'valid input'
+                    $pollname = $query.split('"')[1]
+                    $pollanswers = @()
+                    $answerlist = @()
+                    for($j = 3; $query[$j] -ne $NULL; $j+=2)
+                    {
+                        $pollanswers += $query.split('"')[$j]
+                    }
+                    for($j = 0; $pollanswers[$j] -ne $NULL; $j++)
+                    {
+                        $answerlist += ([string]($j+1) + ". " + $pollanswers[$j] + "`r`n")
+                    }
+                    Invoke-RestMethod -Method Post -Uri ($API_URL + "sendMessage?chat_id=" + $updates.result.message[$i].chat.id + "&text=A new poll has been started!  The question is...`r`n`r`n$pollname`r`n`r`n$answerlist")
+                }
+                else 
+                {
+                    Invoke-RestMethod -Method Post -Uri ($API_URL + "sendMessage?chat_id=" + $updates.result.message[$i].chat.id + '&text=Invalid poll syntax. The format is /newpoll "Poll Name" "Option1" "Option2"...')
+                }
             } 
             "/vote *" # /vote <num of option>.  only can vote on the current poll as only one poll is allowed at a time
-            {
+            {   
                 $query = $updates.result.message[$i].text.substring(6)
-                write-host $query
+                if($query -match '^[1-9]$')
+                {
+
+                }
+                else
+                {
+                    Invoke-RestMethod -Method Post -Uri ($API_URL + "sendMessage?chat_id=" + $updates.result.message[$i].chat.id + "&text=Invalid vote.  The vote command only accepts integers between 1 and 9.")
+                }
             }
-       
-            
+            "/results" # get results of current poll
+            {
+
+            }
         }
 
 
